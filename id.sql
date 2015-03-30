4669cc9d47d63d796332997c63349fd4f2c7d4db
Sergey Pushkin <pushkinsv@gmail.com>
2015-03-30 09:29:34 +0000

    уникальные int8 идентификаторы
    git-svn-id: file:///home/spu/idsvn@4 eb6a42a9-de8b-4275-a949-02ac4ff80a4b

--- id.sql
+++ id.sql
@@ -1,21 +1,24 @@
--- dbext:profile=spu_ztest:
-
-select 1;
-
-drop function systemid();
-create or replace function systemid() returns int as 
+-- Генерация уникальных int8 идентификаторов
+create or replace function systemid() returns int as
 $$
-	select 123
+	select 128
 $$
 language sql immutable;
-drop sequence id_helper;
+
 create sequence id_helper minvalue 0 maxvalue 65535 cycle;
 
-create or replace function generate_id() returns int8 as 
+create or replace function generate_id() returns int8 as
 $$
 	select ((100*extract(epoch from clock_timestamp()))::int8::bit(40)||nextval('id_helper')::bit(12)||systemid()::bit(12))::bit(64)::int8;
 $$
 language sql volatile;
+
+create or replace function generate_id(at_time timestamp with time zone) returns int8 as
+$$
+	select ((100*extract(epoch from at_time))::int8::bit(40)||nextval('id_helper')::bit(12)||systemid()::bit(12))::bit(64)::int8;
+$$
+language sql volatile;
+comment on function generate_id(timestamp with time zone) is 'Внимание: для каждого момента времени может быть сгенерировано только 4096 уникальных идентификаторов';
 
 create or replace function id_of_timestamp(t timestamp with time zone) returns int8 as
 $$
@@ -28,9 +31,5 @@ $$
 	select timestamp with time zone 'epoch' + substring(i::bit(64) from 1 for 40)::int8::double precision/100*interval '1 second';
 $$
 language sql immutable;
-
-select timestamp with time zone 'epoch' + substring(2348890907203338240::bit(64) from 1 for 40)::int8::double precision/100*interval '1 second';
-
-select current_timestamp,id_of_timestamp(current_timestamp),timestamp_of_id(id_of_timestamp(current_timestamp));
 
 
