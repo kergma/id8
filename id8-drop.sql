a13dca4cfb48ac46584a2a85460df08b0e442245
Sergey Pushkin <noc@acs.com>
2024-06-17 16:41:21 +0700

    id8 drop statements

---
+++ id8-drop.sql
@@ -0,0 +1,8 @@
+drop function id8generator();
+drop sequence id_helper;
+drop function generate_id();
+drop function generate_id(timestamp with time zone);
+drop function id_of_timestamp(timestamp with time zone);
+drop function timestamp_of_id(int8);
+drop function generator_of_id(int8);
+drop function isid(anyelement);
